914110fc3482f5fef8719ea4dca565aaa4f31e18
toidiu <toidiu@toidiuMac.nyc.rr.com>
2017-11-07 21:07:39 -0500

    git and also base 16 ocean

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 ln -s .dotfiles/inputrc .inputrc;
 ln -s .dotfiles/vim/vimrc .vimrc;
 ln -s .dotfiles/vim .vim;
+
+#git
+ln -s .dotfiles/git/gitconfig .gitconfig;
+git config --global core.excludesfile ~/.gitignore_global
+ln -s .dotfiles/git/gitignore_global .gitignore_global;
