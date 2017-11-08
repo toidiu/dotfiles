e0b82f6a837d3036baeda6e74e7321dfdf87f8fd
toidiu <toidiu@toidiuMac.nyc.rr.com>
2017-11-07 21:24:21 -0500

    bash stuff

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 ln -s .dotfiles/inputrc .inputrc;
-ln -s .dotfiles/vim/vimrc .vimrc;
 ln -s .dotfiles/vim .vim;
+ln -s .dotfiles/vim/vimrc .vimrc;
 
 #git
 ln -s .dotfiles/git/gitconfig .gitconfig;
 git config --global core.excludesfile ~/.gitignore_global
 ln -s .dotfiles/git/gitignore_global .gitignore_global;
+
+#bash
+ln -s .dotfiles/bash/bashrc .bashrc;
+ln -s .dotfiles/bash/bash_profile .bash_profile;
+
