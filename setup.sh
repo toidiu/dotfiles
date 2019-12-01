d3bcf76233dfc7a4efe1de5274c2ce4a03558e4b
Apoorv Kothari <apoorvko@amazon.com>
2019-12-01 15:10:14 -0800

    fix

--- setup.sh
+++ setup.sh
@@ -1,65 +1,63 @@
 if [[ $USER == "apoorvko" ]]; then
-  if [ -f ~/.is_cloud ]; then # amz cloud
-    echo "amz cloud"
-  else # amz
-    echo "amz"
+  if [ -f ~/.is_cloud ];
+    then echo "amz cloud"
+    else echo "amz";
   fi
-else # home
-  echo "home"
+  else echo "home";
 fi
 
-#common
-if [ $USER == "apoorvko" ] && [ -f ~/.is_cloud ]; then # amz cloud
-  ln -si .dotfiles/cloud/commonrc .commonrc;
-else # home
-  ln -si .dotfiles/commonrc .commonrc;
-fi
+##common
+#if [ $USER == "apoorvko" ] && [ -f ~/.is_cloud ]; then # amz cloud
+#  ln -si .dotfiles/cloud/commonrc .commonrc;
+#else # home
+#  ln -si .dotfiles/commonrc .commonrc;
+#fi
 
-ln -si .dotfiles/vim .vim;
-if [ $USER == "apoorvko" ] && [ -f ~/.is_cloud ]; then # amz cloud
-  ln -si .dotfiles/cloud/vimrc .vimrc;
-else # home
-  ln -si .dotfiles/vim/vimrc .vimrc;
-fi
+#ln -si .dotfiles/vim .vim;
+#if [ $USER == "apoorvko" ] && [ -f ~/.is_cloud ]; then # amz cloud
+#  ln -si .dotfiles/cloud/vimrc .vimrc;
+#else # home
+#  ln -si .dotfiles/vim/vimrc .vimrc;
+#fi
 
-#git
-if [[ $USER == "apoorvko" ]]; then
-  if [ -f ~/.is_cloud ]; then
-    ln -si .dotfiles/cloud/gitconfig_amz .gitconfig;
-  else
-    ln -si .dotfiles/git/gitconfig_amz .gitconfig;
-  fi
-else
-  ln -si .dotfiles/git/gitconfig .gitconfig;
-fi
+##git
+#if [[ $USER == "apoorvko" ]]; then
+#  if [ -f ~/.is_cloud ]; then
+#    ln -si .dotfiles/cloud/gitconfig_amz .gitconfig;
+#  else
+#    ln -si .dotfiles/git/gitconfig_amz .gitconfig;
+#  fi
+#else
+#  ln -si .dotfiles/git/gitconfig .gitconfig;
+#fi
 
-#gitignore_global
-git config --global core.excludesfile ~/.gitignore_global
-if [ $USER == "apoorvko" ] && [ -f ~/.is_cloud ]; then # amz cloud
-  ln -si .dotfiles/cloud/gitignore_global .gitignore_global;
-else # home
-  ln -si .dotfiles/git/gitignore_global .gitignore_global;
-fi
+##gitignore_global
+#git config --global core.excludesfile ~/.gitignore_global
+#if [ $USER == "apoorvko" ] && [ -f ~/.is_cloud ]; then # amz cloud
+#  ln -si .dotfiles/cloud/gitignore_global .gitignore_global;
+#else # home
+#  ln -si .dotfiles/git/gitignore_global .gitignore_global;
+#fi
 
-#bash
-ln -si .dotfiles/bash/bashrc .bashrc;
-ln -si .dotfiles/bash/bash_profile .bash_profile;
-ln -si .dotfiles/inputrc .inputrc;
+##bash
+#ln -si .dotfiles/bash/bashrc .bashrc;
+#ln -si .dotfiles/bash/bash_profile .bash_profile;
+#ln -si .dotfiles/inputrc .inputrc;
 
-#zsh
-if [ $USER == "apoorvko" ] && [ -f ~/.is_cloud ]; then # amz cloud
-  ln -si .dotfiles/cloud/zshrc .zshrc;
-else # home
-  ln -si .dotfiles/zsh/zshrc .zshrc;
-fi
+##zsh
+#if [ $USER == "apoorvko" ] && [ -f ~/.is_cloud ]; then # amz cloud
+#  ln -si .dotfiles/cloud/zshrc .zshrc;
+#else # home
+#  ln -si .dotfiles/zsh/zshrc .zshrc;
+#fi
 
-#tmux
-if [ $USER == "apoorvko" ] && [ -f ~/.is_cloud ]; then # amz cloud
-  ln -si .dotfiles/cloud/tmux.conf .tmux.conf;
-else # home
-  ln -si .dotfiles/tmux.conf .tmux.conf;
-fi
+##tmux
+#if [ $USER == "apoorvko" ] && [ -f ~/.is_cloud ]; then # amz cloud
+#  ln -si .dotfiles/cloud/tmux.conf .tmux.conf;
+#else # home
+#  ln -si .dotfiles/tmux.conf .tmux.conf;
+#fi
 
-#ctags
-ln -si .dotfiles/ctags .ctags;
+##ctags
+#ln -si .dotfiles/ctags .ctags;
 
